db8685451cd65bb90b6e349ed33a2bdf0f9b9017
Bloom Verga <bloomify@vivaldi.net>
2021-12-01 19:53:32 +0100

    Adding SplashScreen

--- App.js
+++ App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { CryptoDetail, Transaction } from "./screens";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen'
 
 import Tabs from "./navigation/tabs";
 
 const Stack = createStackNavigator();
 
 const App = () => {
+
+  React.useEffect(()=> {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <NavigationContainer>
       <Stack.Navigator
